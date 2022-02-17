48976e040f0a5b12ffba621004e6d4fbeb5b2d02
Zacks <Zacks.Shen@gmail.com>
2022-02-17 00:31:50 -0800

    Add output function to leetcode_sql.py

--- LeetCode/leetcode_sql.py
+++ LeetCode/leetcode_sql.py
@@ -11,7 +11,7 @@
 # Email: <zacks.shen@gmail.com>                                                #
 # Github: https://github.com/ZacksAmber                                        #
 # -----                                                                        #
-# Last Modified: 2022-02-12 10:35:07 pm                                        #
+# Last Modified: 2022-02-17 12:22:19 am                                        #
 # Modified By: Zacks Shen <zacks.shen@gmail.com>                               #
 # -----                                                                        #
 # Copyright (c) 2022 Zacks Shen                                                #
@@ -22,30 +22,37 @@ import os
 import json
 
 
-filename = 'leetcode_sql.json'
+inputfile = 'leetcode_sql.json'
+outputfile = 'output.txt'
 
 
 def parser():
-    with open(filename, 'r') as f:
+    with open(inputfile, 'r') as f:
         sql = json.load(f)
-    for tb in sql['rows']:
-        for row in sql['rows'][tb]:
-            print(f"""insert into {tb} ({', '.join(sql['headers'][tb])}) values ({', '.join(map(lambda x: '"' + str(x) + '"', row))});""")
+    with open(outputfile, 'a') as f:
+        for tb, rows in sql['rows'].items():
+            for row in rows:
+                columns = ', '.join(sql['headers'][tb])
+                values = ', '.join(map(lambda x: f'"{x}"', row))
+                insert_statement = f"""insert into {tb} ({columns}) values ({values});"""
+                f.write(insert_statement + "\n")
 
 
 def main():
-    if os.path.exists(filename):
+    if os.path.exists(outputfile):
+        os.remove(outputfile)
+    if os.path.exists(inputfile):
         if os.path.getsize('leetcode_sql.json'):
             parser()
         else:
             print(
-                f'Please paste the SQL statement from your LeetCode test case to {filename}'
+                f'Please paste the SQL statement from your LeetCode test case to {inputfile}'
             )
     else:
-        with open(filename, 'w') as f:
+        with open(inputfile, 'w') as f:
             f.write('')
             print('The program has created a json file for you at the same directory.')
-            print(f'Please paste the SQL statement from your LeetCode test case to {filename}')
+            print(f'Please paste the SQL statement from your LeetCode test case to {inputfile}')
 
 
 if __name__ == '__main__':
