96b6368de5b5006b56243b59e6d7c6deb2ae94c3
Zacks <Zacks.Shen@gmail.com>
2022-03-08 15:36:37 -0800

    Fix bug & add function to remove duplicates such xx 2.doc.icloud

--- iCloud/remove_duplicates.py
+++ iCloud/remove_duplicates.py
@@ -11,7 +11,7 @@
 # Email: <zacks.shen@gmail.com>                                                #
 # Github: https://github.com/ZacksAmber                                        #
 # -----                                                                        #
-# Last Modified: 2022-02-15 12:44:31 pm                                        #
+# Last Modified: 2022-03-08 3:35:39 pm                                         #
 # Modified By: Zacks Shen <zacks.shen@gmail.com>                               #
 # -----                                                                        #
 # Copyright (c) 2022 Zacks Shen                                                #
@@ -38,14 +38,37 @@ def remove_duplicates(dirpath, filenames, mark=2):
         else:
             new_files.append(f)
 
+    # duplicates may be:
+    # - xx 2.doc
+    # - xx.yy 2.doc
+    # - xx 2
+    # - .xx 2.doc.icloud
+    # - xx.yy 2.doc.icloud
+    # - xx 2.icloud
     for duplicate in duplicates:
+        # try:
+        #     filename, ext = duplicate.rsplit('.', 1)
+        #     filename = filename.rsplit(' 2')[0]
+        #     file = filename + '.' + ext
+        # except ValueError:
+        #     filename = duplicate.rsplit(' 2')[0]
+        #     file = filename
         try:
-            filename, ext = duplicate.rsplit('.', 1)
-            filename = filename.rsplit(' 2')[0]
-            file = filename + '.' + ext
+            filename, ext = duplicate.rsplit(' 2', 1)
+            if '.icloud' in ext:
+                file = filename + ext
+                file = file.split('.icloud')[0] # remove .icloud
+                file = file.split('.', 1)[1] # remove ., which makes files hidden
+            else:
+                file = filename + ext
         except ValueError:
             filename = duplicate.rsplit(' 2')[0]
             file = filename
+
+        # Test only
+        # print("duplicate:", duplicate)
+        # print("file:", file)
+        # print(file in new_files)
 
         if file in new_files:
             file_size = os.path.getsize(file)
@@ -57,6 +80,7 @@ def remove_duplicates(dirpath, filenames, mark=2):
             # Test only
             # print(file, file_size)
             # print(duplicate, duplicate_size)
+
             # Remove the old files retrived from iCloud since
             # new files may be larger than old files.
             if (file_size >= duplicate_size) | (file_mtime > duplicate_mtime):
@@ -78,7 +102,7 @@ def main():
     if len(sys.argv) - 1 > 2:
         raise TypeError(f"This program takes 2 positional argument but {len(sys.argv) - 1} were given")
     elif len(sys.argv) - 1 == 0:
-        raise TypeError("This program is required to pass a path such as 'Users/username/Desktop'")
+        raise TypeError("This program is required to pass a path such as '/Users/username/Desktop'")
 
     path = sys.argv[1]
     try:
